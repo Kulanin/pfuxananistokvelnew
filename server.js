557843d578ddadbbbdf8bf79212954396b340398
Kulani Ngobeni <kulanin@net1.com>
2021-01-13 02:10:00 +0200

    kulani

--- server.js
+++ server.js
@@ -668,13 +668,13 @@ const PORT_Number = process.env.PORT || 5001;
 // }
 
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('/build'));
   
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
-  }
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+//     });
+//   }
   
 
 const server = app.listen(PORT_Number, () => {
